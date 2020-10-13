a0d13c7a7952c6f7e0399b4d95638b6509c68abc
Yifan Zhao <44476753+Joy3van@users.noreply.github.com>
2020-10-13 10:41:00 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,127 @@
+#!/usr/bin/env node
+
+/**
+ * Helpful reference: https://developer.mozilla.org/en-US/docs/Web/API/Response/status
+ * https://stackoverflow.com/questions/9153571/is-there-a-way-to-get-version-from-package-json-in-nodejs-code
+ * https://stackoverflow.com/questions/1335851/what-does-use-strict-do-in-javascript-and-what-is-the-reasoning-behind-it
+ */
+'use strict'; // Invoke Strict Mode. //Q: What is some best practice to use strict mode? Should I always use it since it secures my code more?
+const chalk = require("chalk"); //Coloring the text
+const boxen = require("boxen"); //Give the text a box
+const yargs = require("yargs"); //Used for displaying commmand-line help file
+const fs = require("fs");
+const axios = require("axios").default;
+const {
+    version
+} = require('./package.json');
+/*According to this post(https://stackoverflow.com/questions/9153571/is-there-a-way-to-get-version-from-package-json-in-nodejs-code) 
+                                           this is not a secure way to get the version number as the .json file may expose to client, but I didn't find any better solution, leave for improvement in the future*/
+// Globally declared regex for url, credit to Daveo @ https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url
+var pattern = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gim;
+var urlRegex = new RegExp(pattern);
+// Set up CLI flags using yargs
+const options = yargs
+    .usage('Usage: linkedout [OPTION]... URL...\n' + '\nA tiny tool for checking link\'s availiablity')
+    .showHelpOnFail(false, 'Specify --help or -h for available options')
+    .version('v', `linkedout ${version}`)
+    .options({
+        'f': {
+            alias: 'file',
+            describe: 'Check multiple links within a file'
+        },
+        'v': {
+            alias: 'version'
+        },
+        'h': {
+            alias: 'help'
+        },
+        'a': {
+            alias: 'address',
+            describe: 'Check if a single link is working',
+            type: 'string'
+        }
+
+    })
+    .strictOptions() // Display showHelpOnFail message if a non-exist option is input
+    .argv;
+//Main part of the program
+//TODO: Figure out how to use switch case properly here(or it's not applicable here?)
+if (options.file) {
+    linkedInFile(options.file);
+} else if (options.address) {
+    linkCheck(options.address);
+} else {
+    yargs.showHelp();
+}
+
+// function definitions
+// 
+/**
+ * printMsg function
+ * @param {string} status
+ * @param {string} link
+ * Used for printing messages for different scenarios. Using a function makes it
+ * easier for future changes(may add different messages for more status codes)
+ */
+function printMsg(status, link) {
+    switch (status) {
+        case 'good':
+            console.log(chalk.green(link + chalk.bold('\t[GOOD! LINKED address still IN the world!]')));
+            break;
+        case 'bad':
+            console.log(chalk.red(link + chalk.bold(" [BAD! LINKED address is OUT of this dimension :(]")));
+            break;
+            //print the same message for other other status and codes that not 200\400\404 temperorily
+        default:
+            console.log(chalk.grey(link + chalk.bold(" [UNKNOWN...you LINKED an address to Knowhere?]")));
+            break;
+    }
+}
+/**
+ * linkCheck function
+ * @param {string} link 
+ * one thing interesting about this function, when testing this function only, if I test multiple links
+ * e.g. Giving: 
+ * linkCheck('https://developer.mozilla.org/en-US/docs/Web/API/Location/toString');
+ * linkCheck('http://localhost:8080/')
+ * linkCheck('https://github.com/joy3van123')
+ * "localhost" link's message would always be the first line no matter where it is.
+ */
+
+function linkCheck(link) {
+    axios.head(link) // only request headers
+        .then(response => {
+            if (response.status === 200)
+                printMsg('good', link);
+        })
+        .catch(error => {
+            if (error.response) {
+                if (error.response.status === 400 || error.response.status === 404) {
+                    printMsg('bad', link);
+                }
+            } else {
+                printMsg(error, link);
+            }
+        });
+}
+
+/**
+ * linkedInFile function
+ * @param {string} file
+ * Check the link you linkedin a file
+ * 
+ */
+
+function linkedInFile(file) {
+    var readable = fs.createReadStream(file); //Requiring less resource than readFile()
+    readable.setEncoding('utf8'); //Return string instead of Buffer
+    readable.on('data', (chunky) => {
+        let urlArr = chunky.match(urlRegex);
+        urlArr.forEach(url => {
+            linkCheck(url);
+        });
+    });
+    readable.on('error', (err) => {
+        console.log(err);
+    });
+}
